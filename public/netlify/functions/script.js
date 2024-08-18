6a3fa34830ea75f525d8abe87a8945771b010b43
Riyaz Ahmed <riyaz04ahmed24aug.github@gmail.com>
2024-08-19 00:07:10 +0530

    Hosted site

--- public/netlify/functions/script.js
+++ public/netlify/functions/script.js
@@ -4,21 +4,24 @@ if (navigator.geolocation) {
   navigator.geolocation.watchPosition(
     (position) => {
       const { latitude, longitude } = position.coords;
-
       socket.emit("send-location", { latitude, longitude });
     },
     (error) => {
-      console.log(error);
+      console.error("Geolocation error:", error.message);
     },
     {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0,
+      enableHighAccuracy: false, // Try setting this to false
+      timeout: 10000, // Increase timeout
+      maximumAge: 5000, // Allow cached position up to 5 seconds old
     }
   );
+  
+} else {
+  console.error("Geolocation is not supported by this browser.");
 }
 
-const map = L.map("map").setView([0, 0], 16);
+
+const map = L.map("map").setView([0, 0], 10);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "FindULoc",
