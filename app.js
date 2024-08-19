c3bcf8f048d0e592aac8fa1e353b2f0c65ad8d5b
Riyaz Ahmed <riyaz04ahmed24aug.github@gmail.com>
2024-08-19 22:49:29 +0530

    Finished Deployment

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
   socket.on("send-location", (data) => {
